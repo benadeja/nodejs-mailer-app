14770091988980112615a1e8fc2729584d338fa9
jbenade <jaco.benade@momentum.co.za>
2019-06-13 17:46:23 +0200

    Generate React-App

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,7 @@
 const passport = require("passport");
 
 module.exports = app => {
-  app.get(
-    "/auth/google",
+  app.get("/auth/google",
     passport.authenticate("google", {
       scope: ["profile", "email"]
     })
